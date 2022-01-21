cc1298e6ecb73b8cd4e848f0ca17226f93fe4349
Viktor Meglenovski <viktor.meglenovski@gmail.com>
2022-01-21 14:27:24 +0100

    Review microservice implementation
    The reviews microservice has been implemented, connected with the database and tested accordingly.
    
    Co-Authored-By: miteva-h <84291653+miteva-h@users.noreply.github.com>
    Co-Authored-By: Emilija Simonoska <53193642+Emilija-S@users.noreply.github.com>

---
+++ Homework 4/ReviewsMicroservice/ReviewsMicroservice/Controllers/ReviewController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Ajax.Utilities;
+using ReviewsMicroservice.Models;
+using ReviewsMicroservice.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Helpers;
+using System.Web.Http;
+
+namespace ReviewsMicroservice.Controllers
+{
+    public class ReviewController : ApiController
+    {
+        private ReviewService reviewService { get; set; }
+        public ReviewController()
+        {
+            this.reviewService = ReviewService.ReviewServiceInstance();
+        }
+
+        //GET акција која ги враќа сите детали за Review со ID зададено како параметар
+        [HttpGet]
+        [Route("api/review")]
+        public ReviewDetails GetReviewDetails(int id)
+        {
+            return reviewService.getReviewDetails(id);
+        }
+
+        //POST акција за додавање на нов Review или ажурирање на постоечки Review за соодветниот паркинг со соодветните атрибути од тековно најавениот корисник
+        [HttpPost]
+        [Route("api/review")]
+        public void AddOrEditReview(string userId, int parkingId, int stars, string reviewText)
+        {
+            //Се повикува соодветниот метод од сервисот
+            reviewService.addOrEditReview(userId, parkingId, stars, reviewText);
+        }
+
+        //GET акција за бришење на веќе постоечко Review за паркинг со ID дадено како аргумент, напишано од тековно најавениот корисник
+        [HttpGet]
+        [Route("api/review/delete")]
+        public void DeleteReview(string userId, int parkingId)
+        {
+            //Се повикува соодветниот метод од сервисот
+            reviewService.deleteReview(userId, parkingId);
+        }
+    }
+}
---
+++ Homework 4/ReviewsMicroservice/ReviewsMicroservice/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ReviewsMicroservice.Models
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Review> Reviews { get; set; }
+        public AppDbContext() : base("DefaultConnection", throwIfV1Schema: false) { }
+        public static AppDbContext Create()
+        {
+            return new AppDbContext();
+        }
+    }
+}
